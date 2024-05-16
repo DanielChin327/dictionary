d89864c0933faf35ae9b906086103d19c5db602d
Daniel Chin <DanChin327@gmail.com>
2024-05-16 14:27:08 -0600

    added initial js and html. No changes from original.

---
+++ script.js
@@ -0,0 +1,19 @@
+const apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+
+
+
+const displayDefinition = (event) => {
+  event.preventDefault();
+  const word = document.getElementById("word").value;
+  // TODO: Display your age with an AJAX call instead of the console.log()
+  fetch(apiUrl + word)
+    .then(response => response.json())
+    .then(data => {
+      const definition = data[0].meanings[0].definitions[0].definition;
+      const definitionElement = document.getElementById("definition");
+      definitionElement.innerHTML = definition;
+    })
+}
+
+const form = document.getElementById("fetch-word");
+form.addEventListener("submit", displayDefinition);

e8e41834745c3a1ce8e90d3beb4df2d7ce2b83b2
Daniel Chin <DanChin327@gmail.com>
2024-05-16 16:04:27 -0600

    modified JS to include phonetic and synonyms as well.

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 const apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/"
 
 
-
 const displayDefinition = (event) => {
   event.preventDefault();
-  const word = document.getElementById("word").value;
+  const word = document.querySelector("#word").value;
   // TODO: Display your age with an AJAX call instead of the console.log()
   fetch(apiUrl + word)
     .then(response => response.json())
     .then(data => {
       const definition = data[0].meanings[0].definitions[0].definition;
-      const definitionElement = document.getElementById("definition");
-      definitionElement.innerHTML = definition;
+      const definitionElement = document.querySelector("#definition");
+      definitionElement.innerHTML = "Definition: " + definition;
+
+      const phonetic = data[0].phonetics[0].text;
+      const phoneticElement = document.querySelector("#phonetic");
+      phoneticElement.innerHTML = "Phonetic: " + phonetic;
+
+      const synonyms = data[0].meanings[0].synonyms;
+      const synonymsElement = document.querySelector('#synonyms');
+      if (synonyms && synonyms.length > 0) {
+        synonymsElement.innerHTML = "Synonyms: " + synonyms.join(', ');
+      } else {
+        synonymsElement.innerHTML = "No synonyms available.";
+      }
     })
 }
 
-const form = document.getElementById("fetch-word");
+
+
+const form = document.querySelector("#fetch-word");
 form.addEventListener("submit", displayDefinition);
+// this must be placed AFTER defining dsiplayDefinition. Otherwise there is a reference error.
